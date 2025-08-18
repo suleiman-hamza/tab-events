5e5ddcb5043aa3521c439959169fbfed6f82d9a5
suleiman hamza <hamzasuleiman530@gmail.com>
2025-08-18 01:36:07 +0100

    feat: add Accordion component with items and added iconify/lucide icons

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,23 @@
 <script setup lang="ts">
+import type { AccordionItem } from '@nuxt/ui'
+
+const items = ref<AccordionItem[]>([
+  {
+    label: 'Icons',
+    icon: 'i-lucide-smile',
+    content: 'You have nothing to do, @nuxt/icon will handle it automatically.'
+  },
+  {
+    label: 'Colors',
+    icon: 'i-lucide-swatch-book',
+    content: 'Choose a primary and a neutral color from your Tailwind CSS theme.'
+  },
+  {
+    label: 'Components',
+    icon: 'i-lucide-box',
+    content: 'You can customize components by using the `class` / `ui` props or in your app.config.ts.'
+  }
+])
 </script>
 
 <template>
@@ -11,6 +30,7 @@
       <nuxt-link to="/about">
         About
       </nuxt-link>
+      <UAccordion :items="items" />
     </div>
   </section>
 </template>
