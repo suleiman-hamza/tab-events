0e0e033d8b4185c4e5b1a6ad8a0994dcbe4bd2b7
suleiman hamza <hamzasuleiman530@gmail.com>
2025-08-29 12:25:09 +0100

    feat: add invite page and update auth plugin to log organization deletions, installed resend for sending invitations

---
+++ app/pages/app/invite.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="p-4">
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit accusantium dolores explicabo est repudiandae ratione sed totam architecto! Quod vero officiis culpa quaerat cumque impedit sapiente aspernatur ab quam beatae.</p>
+  </section>
+</template>
--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { organization } from 'better-auth/plugins'
+// import { sendOrganizationInvitation } from ''
 import { schema } from '../database/schema/auth' // your schema definition;
 import { db } from '../utils/drizzle' // your drizzle instance
 
@@ -16,12 +17,17 @@ export const auth = betterAuth({
     disableSessionRefresh: true,
   },
 
-  plugins: [organization({
-    organizationDeletion: {
-      disabled: false,
-      afterDelete: async (data, request) => {
-        await console.error('Organization deleted', data.organization.name)
+  plugins: [
+    organization({
+      organizationDeletion: {
+        disabled: false,
+        afterDelete: async (data, request) => {
+          await console.error('Organization deleted', data.organization.name)
+        },
       },
-    },
-  })],
+    }),
+    // async sendInvitationEmail() {
+
+    // }
+  ],
 })
