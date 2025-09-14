7a805675aa6e6bd9636546a043f53aa725ff51ba
suleiman hamza <hamzasuleiman530@gmail.com>
2025-09-14 22:17:39 +0100

    wip: left nav of dashboard header, profile btn, notif btn, search btn

--- app/components/DashboardHeader.vue
+++ app/components/DashboardHeader.vue
@@ -1,5 +1,25 @@
+<script setup lang="ts">
+const profileItems = ref([
+  {
+    label: 'Dashboard',
+    icon: 'i-lucide-circle-plus',
+  },
+  {
+    label: 'Setting',
+    icon: 'i-lucide-cog',
+  },
+])
+</script>
+
 <template>
-  <nav class="border w-full border-red-500 h-auto p-3">
+  <nav class="border w-full border-red-500 h-auto p-3 flex justify-between items-center">
     <h2>Home</h2>
+    <div class="flex gap-4">
+      <UButton label="S" variant="soft" class="flex items-center justify-center rounded-full w-10 h-10" />
+      <UButton label="N" variant="soft" class="flex items-center justify-center rounded-full w-10 h-10" />
+      <UDropdownMenu :items="profileItems">
+        <UButton label="P" variant="soft" class="flex items-center justify-center rounded-full w-10 h-10" />
+      </UDropdownMenu>
+    </div>
   </nav>
 </template>
