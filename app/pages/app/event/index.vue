a6f692e9390dd563842032ff64fb35a411933913
suleiman hamza <hamzasuleiman530@gmail.com>
2025-09-23 12:49:20 +0100

    feat: user menu in navbar w/ popover

--- app/pages/app/event/index.vue
+++ app/pages/app/event/index.vue
@@ -1,5 +1,29 @@
 <script setup lang="ts">
-
+const { user } = useAuth()
+const items = ref([
+  [
+    {
+      label: user.value?.name,
+      avatar: {
+        src: user.value?.image || '',
+        alt: user.value?.name || '',
+      },
+      type: 'label',
+    },
+  ],
+  [
+    {
+      label: 'View Profile',
+    },
+    {
+      label: 'Settings',
+    },
+    {
+      label: 'Sign Out',
+      icon: 'i-lucide-log-out',
+    },
+  ],
+])
 </script>
 
 <template>
@@ -10,14 +34,34 @@
           <UDashboardSidebarCollapse />
         </template>
         <template #right>
-          <UButton icon="i-lucide-user" variant="soft" color="neutral" />
           <UButton icon="i-lucide-bell" variant="soft" color="neutral" />
+          <UDropdownMenu :items="items">
+            <UButton icon="i-lucide-user" variant="soft" color="neutral" />
+          </UDropdownMenu>
+          <UPopover>
+            <UButton icon="i-lucide-user" variant="soft" color="neutral" />
+            <template #content>
+              <section class="w-40">
+                <div class="flex gap-2 items-center p-3">
+                  <UAvatar />
+                  <span class="truncate">
+                    <h4>{{ user?.name }}</h4>
+                    <h5 class="text-sm truncate">{{ user?.email }}</h5>
+                  </span>
+                </div>
+                <USeparator />
+                <div class="p-3">
+                  <p>This is random stuff oo, like from the galaxy haha</p>
+                </div>
+              </section>
+            </template>
+          </UPopover>
         </template>
       </UDashboardNavbar>
     </template>
     <template #body>
       <section class="h-screen flex gap-4 border justify-center items-center">
-        <div class="border flex flex-col items-center gap-2 p-4">
+        <div class="flex flex-col items-center gap-2 p-4">
           <Icon name="i-lucide-calendar" size="60" />
           <h3>No Upcoming Events</h3>
           <p>You have no upcoming events. Wanna host one?</p>
