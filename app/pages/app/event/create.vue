84cb38d21566221f7a4100e55e11d9c0a6f3d8fb
suleiman hamza <hamzasuleiman530@gmail.com>
2025-09-26 10:39:05 +0100

    style: create event drop image and select visibility styled

--- app/pages/app/event/create.vue
+++ app/pages/app/event/create.vue
@@ -1,32 +1,51 @@
 <script setup lang="ts">
+import type { SelectItem } from '@nuxt/ui'
+
 const state = reactive({
   name: undefined,
+  file: undefined,
+  visibility: undefined,
 })
 
 const items = ref([
   {
-    label: 'public',
+    label: 'Public',
+    value: 'public',
+    icon: 'i-lucide-globe',
   },
   {
-    label: 'private',
+    label: 'Private',
+    value: 'private',
+    icon: 'i-lucide-circle-plus',
   },
-])
+] satisfies SelectItem[])
+
+const value = ref(items.value[0]?.value)
+
+const icon = computed(() => items.value.find(item => item.value === value.value)?.icon)
 </script>
 
 <template>
   <section>
-    <UPageBody :ui="{ content: 'border' }">
-      <UForm :state>
-        <UFormField label="Name" name="name">
-          <UFileUpload :ui="{ root: 'w-40 border-amber-400' }" />
-        </UFormField>
-        <UFormField>
-          <USelect :items="items" leading-icon="i-lucide-globe" class="w-40" />
-        </UFormField>
-        <UFormField>
-          <UInput placeholder="Event Name" size="xl" :ui="{ base: 'rounded-none px-0' }" variant="ghost" class="text-3xl px-0" />
-        </UFormField>
-      </UForm>
-    </UPageBody>
+    <UForm :state class="flex flex-col gap-4 mb-4 sm:flex-row max-w-6xl mx-auto">
+      <UFormField>
+        <UFileUpload v-model="state.file" icon="i-lucide-image" accept="image/*" label="Drop your image here" description="SVG, PNG, JPG or GIF (max. 2MB)" class="aspect-square mb-4" />
+        <!-- <USelect v-model="visibilityVal" variant="soft" :items="visibility" value-key="value" :leading-icon="icon" class="w-40" /> -->
+        <USelect v-model="value" variant="soft" :items="items" value-key="value" :icon="icon" class="w-40" />
+      </UFormField>
+      <UFormField>
+        <UInput placeholder="Event Name" size="xl" :ui="{ base: 'rounded-none px-0' }" variant="ghost" class="text-3xl px-0" />
+      </UFormField>
+    </UForm>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit doloribus cum atque esse, explicabo exercitationem est dicta reiciendis. Minima, omnis veniam. Modi exercitationem soluta maxime necessitatibus odio. Corrupti, dolore sit.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit doloribus cum atque esse, explicabo exercitationem est dicta reiciendis. Minima, omnis veniam. Modi exercitationem soluta maxime necessitatibus odio. Corrupti, dolore sit.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit doloribus cum atque esse, explicabo exercitationem est dicta reiciendis. Minima, omnis veniam. Modi exercitationem soluta maxime necessitatibus odio. Corrupti, dolore sit.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit doloribus cum atque esse, explicabo exercitationem est dicta reiciendis. Minima, omnis veniam. Modi exercitationem soluta maxime necessitatibus odio. Corrupti, dolore sit.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit doloribus cum atque esse, explicabo exercitationem est dicta reiciendis. Minima, omnis veniam. Modi exercitationem soluta maxime necessitatibus odio. Corrupti, dolore sit.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit doloribus cum atque esse, explicabo exercitationem est dicta reiciendis. Minima, omnis veniam. Modi exercitationem soluta maxime necessitatibus odio. Corrupti, dolore sit.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit doloribus cum atque esse, explicabo exercitationem est dicta reiciendis. Minima, omnis veniam. Modi exercitationem soluta maxime necessitatibus odio. Corrupti, dolore sit.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit doloribus cum atque esse, explicabo exercitationem est dicta reiciendis. Minima, omnis veniam. Modi exercitationem soluta maxime necessitatibus odio. Corrupti, dolore sit.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit doloribus cum atque esse, explicabo exercitationem est dicta reiciendis. Minima, omnis veniam. Modi exercitationem soluta maxime necessitatibus odio. Corrupti, dolore sit.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit doloribus cum atque esse, explicabo exercitationem est dicta reiciendis. Minima, omnis veniam. Modi exercitationem soluta maxime necessitatibus odio. Corrupti, dolore sit.</p>
   </section>
 </template>
