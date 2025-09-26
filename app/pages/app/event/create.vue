d341ddcfdc041a6b9eea00b6a97224443d91e5c9
suleiman hamza <hamzasuleiman530@gmail.com>
2025-09-26 03:28:55 +0100

    feat: vreate event page

---
+++ app/pages/app/event/create.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const state = reactive({
+  name: undefined,
+})
+
+const items = ref([
+  {
+    label: 'public',
+  },
+  {
+    label: 'private',
+  },
+])
+</script>
+
+<template>
+  <section>
+    <UPageBody>
+      <UForm :state>
+        <UFormField label="Name" name="name">
+          <UFileUpload />
+        </UFormField>
+        <UFormField>
+          <UDropdownMenu :items="items">
+            <UButton label="Public" />
+          </UDropdownMenu>
+        </UFormField>
+      </UForm>
+    </UPageBody>
+  </section>
+</template>
