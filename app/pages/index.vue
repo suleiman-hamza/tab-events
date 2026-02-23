88a8eda9a746ea84cde4f8347a57f24ff36b57f5
suleiman hamza <hamzasuleiman530@gmail.com>
2026-02-24 00:40:12 +0100

    feat/wip: supporting sections in landing page, features ad testimonial

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,8 +2,62 @@
 definePageMeta({
   layout: 'default',
 })
+
+const cards = ref([
+  {
+    title: 'Icons',
+    description: 'Nuxt UI integrates with Nuxt Icon to access over 200,000+ icons from Iconify.',
+    icon: 'i-lucide-smile',
+    to: '/docs/getting-started/integrations/icons',
+    image: '/images/logo_svg.svg',
+  },
+  {
+    title: 'Fonts',
+    description: 'Nuxt UI integrates with Nuxt Fonts to provide plug-and-play font optimization.',
+    icon: 'i-lucide-a-large-small',
+    to: '/docs/getting-started/integrations/fonts',
+    image: '/images/logo_svg.svg',
+  },
+  {
+    title: 'Color Mode',
+    description: 'Nuxt UI integrates with Nuxt Color Mode to switch between light and dark.',
+    icon: 'i-lucide-sun-moon',
+    to: '/docs/getting-started/integrations/color-mode',
+    image: '/images/logo_svg.svg',
+  },
+])
 </script>
 
 <template>
   <HeroSection />
+  <UPageSection title="Features" description="Discover the powerful features of our UI library." :ui="{ body: 'border' }">
+    <template #body>
+      <UPageGrid>
+        <UPageCard
+          v-for="(card, index) in cards"
+          :key="index"
+          :ui="{ root: card.image ? 'bg-[url(/images/logo_svg.svg)] bg-no-repeat bg-top-right bg-size-[100px]' : '' }"
+        >
+          <template #default>
+            <div class="">
+              <h3 class="font-semibold text-lg mb-1">
+                {{ card.title }}
+              </h3>
+              <p class="text-sm text-muted-foreground">
+                {{ card.description }}
+              </p>
+            </div>
+          </template>
+        </UPageCard>
+      </UPageGrid>
+    </template>
+  </UPageSection>
+  <UPageSection title="Loved by the community" description="Don't take our word for it - listen to what our community members have to say." orientation="horizontal">
+    <div>
+      <p>
+        40m+
+        Weekly NPM downloads
+      </p>
+    </div>
+  </UPageSection>
 </template>
